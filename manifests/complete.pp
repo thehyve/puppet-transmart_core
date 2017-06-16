55ed822d35ba984519c07480d046c25c9a666103
Gijs Kant <gijs@thehyve.nl>
2017-06-16 17:00:07 +0200

    Remove postgres database installation from complete manifest.

--- manifests/complete.pp
+++ manifests/complete.pp
@@ -1,6 +1,5 @@
 class transmart_core::complete inherits transmart_core::params {
     include ::transmart_core
-    include ::transmart_core::database
     include ::transmart_core::data
     include ::transmart_core::config
     include ::transmart_core::solr
