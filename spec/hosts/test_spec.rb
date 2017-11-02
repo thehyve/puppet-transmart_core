0378766ace44a45b17f07c48ee8c70ecdb19fbd0
Gijs Kant <gijs@thehyve.nl>
2017-11-02 10:48:29 +0100

    Whitespace.

--- spec/hosts/test_spec.rb
+++ spec/hosts/test_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe 'test.example.com' do
   on_supported_os.each do |os, facts|
-    context 'transmart_core with database credentials set' do
-      let (:facts) { facts }
+    context "transmart_core with database credentials set on #{os}" do
+      let(:facts) { facts }
       it { is_expected.to create_class('transmart_core') }
     end
   end
