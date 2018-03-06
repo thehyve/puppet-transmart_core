0224aff70f4faa7083ab103d6f77606819e90b89
Gijs Kant <gijs@thehyve.nl>
2018-03-06 21:02:18 +0100

    Skip custom service script for rserve on Ubuntu.

--- manifests/rserve.pp
+++ manifests/rserve.pp
@@ -6,42 +6,47 @@ class transmart_core::rserve inherits transmart_core::params {
     $home = $::transmart_core::params::tsuser_home
     $rserve_script = "${home}/rserve"
 
-    User[$user]
-    -> package { 'transmart-r':
-        ensure => 'latest',
-        notify => Service['transmart-rserve'],
-    }
-    -> file { $rserve_script:
-        ensure => file,
-        owner  => $user,
-        mode   => '0755',
-        source => 'puppet:///modules/transmart_core/rserve',
-    }
-    -> file { '/etc/systemd/system/transmart-rserve.service':
-        ensure  => file,
-        mode    => '0644',
-        content => template('transmart_core/service/transmart-rserve.service.erb'),
-    }
-    ~> service { 'transmart-rserve':
-        ensure   => running,
-        provider => 'systemd',
-        enable   => true,
-    }
-
     case $::osfamily {
         'redhat': {
+            $create_rserve_service = true
             $libpng_pkg = 'libpng12'
         }
         'debian': {
+            $create_rserve_service = false
             $libpng_pkg = 'libpng-dev'
         }
         default: {
+            $create_rserve_service = false
             $libpng_pkg = 'libpng-dev'
         }
     }
 
     # Package required for the R scripts
     package { $libpng_pkg: }
+
+    package { 'transmart-r':
+        ensure => 'latest',
+    }
+
+    if $create_rserve_service {
+        file { $rserve_script:
+            ensure  => file,
+            owner   => $user,
+            mode    => '0755',
+            source  => 'puppet:///modules/transmart_core/rserve',
+            require => User[$user],
+        }
+        -> file { '/etc/systemd/system/transmart-rserve.service':
+            ensure  => file,
+            mode    => '0644',
+            content => template('transmart_core/service/transmart-rserve.service.erb'),
+        }
+        ~> service { 'transmart-rserve':
+            ensure   => running,
+            provider => 'systemd',
+            enable   => true,
+        }
+    }
 
 }
 
