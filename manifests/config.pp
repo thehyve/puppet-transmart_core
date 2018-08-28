32668d8b947c1924e1a37e81bff4959438abdc7c
Gijs Kant <gijs@thehyve.nl>
2018-08-28 09:32:41 +0200

    Enable disabling Keycloak certificate checking for testing.

--- manifests/config.pp
+++ manifests/config.pp
@@ -9,6 +9,11 @@ class transmart_core::config inherits transmart_core::params {
     }
 
     if ($::transmart_core::params::server_type == 'api-server') {
+
+        if ($::transmart_core::params::keycloak_disable_trust_manager) {
+            notify { "Warning: certificate checking for Keycloak on ${::hostname} is disabled! NEVER DO THIS IN PRODUCTION!": }
+        }
+
         file { "${::transmart_core::params::tsuser_home}/transmart-api-server.config.yml":
             ensure  => file,
             content => template('transmart_core/config/transmart-api-server/config.yml.erb'),
